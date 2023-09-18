f14b3d9881696554846e571d24b1a2ba6a9c02bf
Austin Burkhalter <austinburkhalter2@gmail.com>
2023-09-18 09:46:02 -0500

    Create main.py
    Implemented recursive algorithm for solving sudoku puzzles.

---
+++ main.py
@@ -0,0 +1,117 @@
+# Print current state of puzzle
+def print_puzzle(puzzle):
+    for i in range(9):
+        print(puzzle[i])
+        # for j in range(9):
+        #     print(puzzle[i][j])
+        #     if (j == 2) or (j == 5):
+        #         print('\t\t')
+
+
+# Find an empty spot in the puzzle
+def find_blank(puzzle):
+    for i in range(9):
+        for j in range(9):
+            if puzzle[i][j] == -1:
+                return i, j
+
+    return None, None
+
+
+# Verify that a guess does not violate any rules
+def check_guess(puzzle, row, col, guess):
+    if guess in puzzle[row]:
+        return False
+
+    for i in range(9):
+        if puzzle[i][col] == guess:
+            return False
+
+    square_x = (col // 3) * 3
+    square_y = (row // 3) * 3
+    for i in range(square_y, square_y + 3):
+        for j in range(square_x, square_x + 3):
+            if puzzle[i][j] == guess:
+                return False
+
+    return True
+
+
+# Recursively find blanks, make guesses, and backtrack as necessary
+def solve_puzzle(puzzle):
+    row, col = find_blank(puzzle)
+
+    if row is None:
+        print_puzzle(puzzle)
+        return True
+
+    for i in range(1, 10):
+        if check_guess(puzzle, row, col, i):
+            puzzle[row][col] = i
+            if solve_puzzle(puzzle):
+                return True
+
+        puzzle[row][col] = -1
+
+    return False
+
+
+if __name__ == '__main__':
+    puzzle_blank = [
+        [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+
+        [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+
+        [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    ]
+    puzzle_easy = [
+        [-1, -1, 6, -1, -1, -1, 5, -1, 8],
+        [1, -1, 2, 3, 8, -1, -1, -1, 4],
+        [-1, -1, -1, 2, -1, -1, 1, 9, -1],
+
+        [-1, -1, -1, -1, 6, 3, -1, 4, 5],
+        [-1, 6, 3, 4, -1, 5, 8, 7, -1],
+        [5, 4, -1, 9, 2, -1, -1, -1, -1],
+
+        [-1, 8, 7, -1, -1, 4, -1, -1, -1],
+        [2, -1, -1, -1, 9, 8, 4, -1, 7],
+        [4, -1, 9, -1, -1, -1, 3, -1, -1]
+    ]
+    puzzle_unsolvable = [
+        [1, 2, 3, -1, -1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, 4, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1, -1, -1, 4],
+
+        [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1, -1, -1, 1],
+
+        [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    ]
+    puzzle_advanced = [
+        [-1, 9, -1, -1, -1, -1, 8, 3, -1],
+        [-1, -1, 3, 6, -1, -1, -1, 4, -1],
+        [-1, -1, 8, -1, -1, -1, -1, -1, -1],
+
+        [2, -1, -1, -1, 9, -1, -1, -1, -1],
+        [-1, -1, -1, 4, 3, 8, -1, -1, -1],
+        [9, -1, 5, -1, 6, -1, -1, -1, -1],
+
+        [-1, -1, -1, 7, -1, -1, 9, -1, -1],
+        [-1, -1, -1, -1, -1, 4, -1, -1, 6],
+        [1, 7, -1, -1, -1, -1, -1, -1, 5],
+    ]
+
+    print_puzzle(puzzle_advanced)
+    print()
+
+    if not(solve_puzzle(puzzle_advanced)):
+        print("Not solvable")
